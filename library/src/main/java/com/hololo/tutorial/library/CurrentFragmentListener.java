dfa7e6b44fa5b9676307727e60a1566814359713
smhdk <semih.dik@telpa.com>
2018-10-25 16:53:41 +0300

    -Deleted listener

--- library/src/main/java/com/hololo/tutorial/library/CurrentFragmentListener.java
+++
@@ -1,8 +0,0 @@
-package com.hololo.tutorial.library;
-
-import android.support.annotation.Nullable;
-import android.view.View;
-
-public interface CurrentFragmentListener {
-    void currentFragment(@Nullable View view, @Nullable Integer position);
-}
